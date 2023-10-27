0344138d2839b5f3661464004ef667089b9a952a
KimDabomi <cdabomi@nate.com>
2023-10-27 16:08:57 +0900

    oop_study

---
+++ src/Accounting.java
@@ -0,0 +1,16 @@
+class Accounting {
+    public double valueOfSupply;
+    public static double vatRate = 0.1;
+
+    public Accounting(double valueOfSupply) {
+        this.valueOfSupply = valueOfSupply;
+    }
+
+    public double getVAT() {
+        return valueOfSupply * vatRate;
+    }
+
+    public double getTotal() {
+        return valueOfSupply + getVAT();
+    }
+}
